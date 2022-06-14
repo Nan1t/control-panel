b464c9be78ba6e60757b1df5bce67201c2d72d8f
Nanit <nanitmix@gmail.com>
2022-06-14 21:49:05 +0300

    Initial commit

---
+++ src/routes/Login.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/routes/Rendering.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Teachers() {
+    return (
+        <div>
+            <h1>Teachers</h1>
+        </div>
+    );
+}
+
+export default Teachers;
